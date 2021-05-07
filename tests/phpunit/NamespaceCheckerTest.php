f9e93aee97b1cb42a9bada14974ffdab223d5572
Kaspars Dambis <hi@kaspars.net>
2021-05-07 14:40:32 +0300

    Test the transform checker

--- tests/phpunit/NamespaceCheckerTest.php
+++ tests/phpunit/NamespaceCheckerTest.php
@@ -12,4 +12,29 @@ class TestNamespaceChecker extends TestCase {
         $this->assertFalse( NamespaceChecker::isNamespace( 'SomeRandomString' ) );
     }
 
+    public function testShouldTransform() {
+        $namespaces_to_transform = [ 
+            'Vendor\\Package' => true,
+            'NotOur\\Prefix' => true,
+            'Not\\Our\\Prefix' => true,
+        ];
+
+        $checker = new NamespaceChecker( $namespaces_to_transform, 'Our\\Prefix' );
+
+        $this->assertFalse( 
+            $checker->shouldTransform( '\\WP_Post' ),
+            'Skip transforming unknown namespaces'
+        );
+
+        $this->assertTrue( 
+            $checker->shouldTransform( 'Vendor\\Package' ),
+            'Transform listed namespaces'
+        );
+
+        $this->assertFalse( 
+            $checker->shouldTransform( 'Our\\Prefix' ),
+            'Skip transforming our own namespace'
+        );
+    }
+
 }
