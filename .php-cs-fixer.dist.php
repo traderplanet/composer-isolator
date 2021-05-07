3f60e9f03f792a5c88d9c04f0cfa1f8bd387e4e2
Kaspars Dambis <hi@kaspars.net>
2021-05-07 13:54:54 +0300

    Lint composer and update cs API usage

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,13 @@
 <?php
 
-return PhpCsFixer\Config::create()
-    ->setUsingCache(false)
+$config = new PhpCsFixer\Config();
+
+return $config->setUsingCache(false)
     ->setRules([
         '@PSR2' => true,
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'binary_operator_spaces' => ['align_double_arrow' => true, 'align_equals' => null],
+        'binary_operator_spaces' => ['operators' => ['=>' => 'align']],
         'concat_space' => ['spacing' => 'one'],
         'ordered_imports' => true,
 
